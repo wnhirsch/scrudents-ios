b1c39d8e63e28c559584151b6df767ea547ba40a
Wellington Nascente Hirsch <wellington@conectivida.com.br>
2020-10-15 00:37:04 -0300

    Project Creation

---
+++ scrudents/Scrudents/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Scrudents
+//
+//  Created by Wellington Nascente Hirsch on 14/10/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ scrudents/Scrudents/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Scrudents
+//
+//  Created by Wellington Nascente Hirsch on 14/10/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
